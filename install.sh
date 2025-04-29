8826d77e85fdf70a38ee1d750732cad47551c7fe
tf <3728284@qq.com>
2025-04-30 02:59:52 +0800

    755 install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,4 @@ cd sherpa-onnx-ext && \
     export SHERPA_ONNXRUNTIME_LIB_DIR=/$WORKDIR/onnxruntime-linux-x64-gpu-1.18.0/lib && \
     export SHERPA_ONNXRUNTIME_INCLUDE_DIR=/$WORKDIR/onnxruntime-linux-x64-gpu-1.18.0/include && \
     python setup.py install && \
-    python -c "import sherpa_onnx; print('安装的sherpa_onnx版本:', sherpa_onnx.__version__)" && \
-    find /opt/asr -type d -name "__pycache__" -exec rm -rf {} + 2>/dev/null || true
+    python -c "import sherpa_onnx; print('安装的sherpa_onnx版本:', sherpa_onnx.__version__)"
